66380c0010f746cee31624ce2e8e11f9fd0bacb9
shookh_uz <zshahriyor40@gmail.com>
2025-11-10 18:58:59 +0500

    bot

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bot/",
+  // base: "/bot/", // ❌ NOTO'G'RI - Vercel'da muammo
+  base: "/", // ✅ TO'G'RI - Vercel uchun
   optimizeDeps: {
     include: ["swiper"],
   },
   server: {
     host: true,
     port: 5174,
-    allowedHosts: [".ngrok-free.app"],
+    allowedHosts: [".ngrok-free.app", ".ngrok-free.dev"],
   },
 });
